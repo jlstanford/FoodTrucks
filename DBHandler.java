cf56d9c35db151f6899094bb83cf482e5982d945
Jessica <jstanford9307@gmai.com>
2016-12-21 16:19:08 -0500

    Added code to create database tables

--- DBHandler.java
+++ DBHandler.java
@@ -4,17 +4,39 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by Jessica on 12/19/2016.
- */
 public class DBHandler extends SQLiteOpenHelper {
-    public DBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "foodTrucks";
+
+    private static final String TRUCKS_TABLE = "trucks";
+    private static final String TRUCK_NAME = "truckName";
+    private static final String FOOD_TYPE = "foodType";
+    private static final String LOCATION = "location";
+
+    private static final String COUPONS_TABLE = "coupons";
+    private static final String COUPON_NAME = "couponName";
+    private static final String COUPON_ID = "couponID";
+
+    private static final String COUPON_INFO_TABLE = "couponInfo";
+    private static final String COUPON_DESCRIPTION = "couponDescription";
+    private static final String COUPON_CODE = "couponCode";
+
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String CREATE_TRUCKS_TABLE = "CREATE TABLE " + TRUCKS_TABLE + "(" + TRUCK_NAME + " TEXT PRIMARY KEY," + FOOD_TYPE + " TEXT," + LOCATION + " TEXT" + ")";
 
+        String CREATE_COUPONS_TABLE = "CREATE TABLE " + COUPONS_TABLE + "(" + TRUCK_NAME + " TEXT," + COUPON_NAME + " TEXT," + COUPON_ID + " INTEGER UNIQUE," + ")";
+
+        String CREATE_COUPON_INFO_TABLE = "CREATE TABLE " + COUPON_INFO_TABLE + "(" + COUPON_ID +" INTEGER UNIQUE PRIMARY KEY," + COUPON_DESCRIPTION + " TEXT," +COUPON_CODE + " TEXT"+ ")";
+
+        db.execSQL(CREATE_TRUCKS_TABLE);
+        db.execSQL(CREATE_COUPONS_TABLE);
+        db.execSQL(CREATE_COUPON_INFO_TABLE);
     }
 
     @Override
--- Trucks.java
+++ Trucks.java
@@ -4,19 +4,29 @@ package jstan11.foodtrucks;
  * Created by Jessica on 12/21/2016.
  */
 public class Trucks {
-    private String Name;
-    private String Type;
-    private String Location;
+    private String name;
+    private String type;
+    private String location;
+
+   public Trucks(){
+
+   }
+
+    public Trucks(String name, String type, String location){
+        this.name = name;
+        this.type = type;
+        this.location = location;
+    }
 
     public String getName(){
-        return this.Name;
+        return this.name;
     }
 
     public String getType(){
-        return this.Type;
+        return this.type;
     }
 
     public String getLocation(){
-        return this.Location;
+        return this.location;
     }
 }
