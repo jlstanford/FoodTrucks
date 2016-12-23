5cc2bd3fe78750724b37c5a5c0a1fbe05b1b7f15
Jessica <jstanford9307@gmail.com>
2016-12-23 14:15:23 -0600

    Finished addCoupon method

--- DBHandler.java
+++ DBHandler.java
@@ -1,5 +1,6 @@
 package jstan11.foodtrucks;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -14,13 +15,13 @@ public class DBHandler extends SQLiteOpenHelper {
     private static final String FOOD_TYPE = "foodType";
     private static final String LOCATION = "location";
 
-    private static final String COUPONS_TABLE = "coupons";
-    private static final String COUPON_NAME = "couponName";
-    private static final String COUPON_ID = "couponID";
+    private static final String AVAILABLE_TRUCK_COUPONS_TABLE = "availableTruckCoupons";
 
     private static final String COUPON_INFO_TABLE = "couponInfo";
     private static final String COUPON_DESCRIPTION = "couponDescription";
     private static final String COUPON_CODE = "couponCode";
+    private static final String COUPON_NAME = "couponName";
+    private static final String COUPON_ID = "couponID";
 
     public DBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -32,7 +33,7 @@ public class DBHandler extends SQLiteOpenHelper {
 
         String CREATE_COUPONS_TABLE = "CREATE TABLE " + COUPON_INFO_TABLE + "(" + COUPON_ID +" INTEGER UNIQUE PRIMARY KEY," + COUPON_NAME + " TEXT," + COUPON_DESCRIPTION + " TEXT," +COUPON_CODE + " TEXT"+ ")";
 
-        String CREATE_AVAILABLE_TRUCK_COUPONS_TABLE = "CREATE TABLE " + COUPONS_TABLE + "(" + TRUCK_NAME + " TEXT," +  COUPON_ID + " INTEGER" + ")";
+        String CREATE_AVAILABLE_TRUCK_COUPONS_TABLE = "CREATE TABLE " + AVAILABLE_TRUCK_COUPONS_TABLE + "(" + TRUCK_NAME + " TEXT," +  COUPON_ID + " INTEGER" + ")";
 
         db.execSQL(CREATE_TRUCKS_TABLE);
         db.execSQL(CREATE_COUPONS_TABLE);
@@ -42,14 +43,22 @@ public class DBHandler extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TRUCKS_TABLE);
-        db.execSQL("DROP TABLE IF EXISTS " + COUPONS_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + AVAILABLE_TRUCK_COUPONS_TABLE);
         db.execSQL("DROP TABLE IF EXISTS " + COUPON_INFO_TABLE);
-
         onCreate(db);
     }
 
-    public void addCoupon(Coupon coupon){
+    SQLiteDatabase writableDb = this.getWritableDatabase();
 
+    public void addCoupon(Coupon coupon){
+        ContentValues values = new ContentValues();
+        values.put(COUPON_ID,coupon.getCouponId());
+        values.put(COUPON_NAME,coupon.getName());
+        values.put(COUPON_DESCRIPTION,coupon.getDescription());
+        values.put(COUPON_CODE,coupon.getCode());
+
+        writableDb.insert(COUPON_INFO_TABLE,null,values);
+        writableDb.close();
     }
 
     public void addTruck(Truck truck){}
